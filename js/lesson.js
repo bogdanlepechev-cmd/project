22df0f7557e7c49a5b2d5fe65ccb1dc3450a2858
bogdanlepechev-cmd <bogdanlepechev@gmail.com>
2026-07-08 00:06:59 +0600

    сделал конвертер

--- js/lesson.js
+++ js/lesson.js
@@ -38,10 +38,46 @@ tabParent.onclick = (event) => {
             if (item === event.target) {
                 hideTabContent();
                 showTabContent(index);
-                
                 clearInterval(intervalId);
                 intervalId = setInterval(nextTab, 3000);
             }
         });
     }
+};
+
+// CONVERTER
+
+const somInput = document.querySelector('#som');
+const usdInput = document.querySelector('#usd');
+const eurInput = document.querySelector('#eur');
+
+const xhr = new XMLHttpRequest();
+xhr.open('GET', '../data/converter.json');
+xhr.send();
+
+xhr.onload = () => {
+    const data = JSON.parse(xhr.response);
+    const convert = (input) => {
+        const value = input.value;
+        if (value === '') {
+            somInput.value = '';
+            usdInput.value = '';
+            eurInput.value = '';
+            return;
+        }
+        const numValue = parseFloat(value);
+        if (input.id === 'som') {
+            usdInput.value = (numValue / data.usd).toFixed(2);
+            eurInput.value = (numValue / data.eur).toFixed(2);
+        } else if (input.id === 'usd') {
+            somInput.value = (numValue * data.usd).toFixed(2);
+            eurInput.value = ((numValue * data.usd) / data.eur).toFixed(2);
+        } else if (input.id === 'eur') {
+            somInput.value = (numValue * data.eur).toFixed(2);
+            usdInput.value = ((numValue * data.eur) / data.usd).toFixed(2);
+        }
+    };
+    somInput.oninput = () => convert(somInput);
+    usdInput.oninput = () => convert(usdInput);
+    eurInput.oninput = () => convert(eurInput);
 };
